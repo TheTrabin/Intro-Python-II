3d411aa239de220f9e3404d480af729532a79c5e
TheTrabin <beyondtears@gmail.com>
2020-09-10 04:47:42 +0000

    Bases set, mvp for ItoP3

--- src/player.py
+++ src/player.py
@@ -1,2 +1,16 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    def __init__(self, name, starting_room):
+        self.name = name
+        self.current_room = starting_room
+
+    def __str__(self):
+        player_string = f"(player.name) is currently in (self.current_room)"
+
+    def move(self, direction):
+        next_room = self.current_room.getdirection(direction)
+        if next_room is not None:
+            self.current_room = next_room
+        else:
+            print("This isn't an exit. Please input again")
